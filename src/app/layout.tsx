5e75c5bee441af1ad4df991792fed872bab50fd9
Moinuddin-dotcom <moinchy7@gmail.com>
2025-08-28 18:05:49 +0600

    package.json conflict resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import type { Metadata } from "next";
-import "./globals.css";
-import AppSidebar from "@/components/AppSidebar/AppSidebar";
-import Navabar from "@/components/DashboardNav/Navabar";
-=======
 import "./globals.css"
 import type { Metadata } from "next"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
->>>>>>> 815f204682d7667da8f3f623dfb0cd30670a4552
 
 export const metadata: Metadata = {
   title: "Guptodhan Dashboard",
