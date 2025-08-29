07f65b4fb00c2f2003ce1a5861fcc063f5b62e60
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-08-29 22:26:25 +0600

    size page edited

--- src/app/general/view/all/sizes/page.tsx
+++ src/app/general/view/all/sizes/page.tsx
@@ -3,7 +3,7 @@ export default function ViewAllSizesPage() {
     <div className="container mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">All Sizes</h1>
       <div className="bg-white rounded-lg shadow p-6">
-        <p>Size management page content will go here.</p>
+        <p>Size management page </p>
       </div>
     </div>
   );
