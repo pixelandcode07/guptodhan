402c8d6d15b67fc318b88599a18ab35100c5f1aa
naeem majumder <naeemmajumder00@gmail.com>
2025-09-08 23:03:03 +0600

    product config

---
+++ src/lib/modules/product-config/interfaces/productFlag.interface.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export interface IProductFlag extends Document {
+  productFlagId: string;
+  name: string;
+  icon: string;
+  status: 'active' | 'inactive';
+  featured: boolean;
+  createdAt: Date;
+}
---
+++ src/lib/modules/product-config/interfaces/productSize.interface.ts
@@ -0,0 +1,8 @@
+import { Document } from 'mongoose';
+
+export interface IProductSize extends Document {
+  sizeId: string;             
+  name: string;               
+  status: 'active' | 'inactive'; 
+  createdAt: Date;           
+}
