ab609a6d31476977a2a6c1d46c4dbecaad5c99e5
naeem majumder <naeemmajumder00@gmail.com>
2025-09-14 22:11:33 +0600

    vendor category

---
+++ src/lib/modules/vendor-category/models/vendorCategory.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model, models } from 'mongoose';
+import { ICategory } from '../interfaces/vendorCategory.interface';
+
+const categorySchema = new Schema<ICategory>(
+  {
+    categoryId: { type: String, required: true, unique: true },
+    name: { type: String, required: true, trim: true },
+    categoryIcon: { type: String, required: true },
+    categoryBanner: { type: String },
+    isFeatured: { type: Boolean, default: false },
+    isNavbar: { type: Boolean, default: false },
+    slug: { type: String, required: true, unique: true },
+    status: { type: String, enum: ['active', 'inactive'], default: 'active' },
+  },
+  { timestamps: { createdAt: true, updatedAt: false } }
+);
+
+export const CategoryModel =
+  models.CategoryModel || model<ICategory>('CategoryModel', categorySchema);
