131d385d5420c9636f41a1deaa7c41cadeb13985
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-09-16 22:03:19 +0600

    haf product details page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
-import Feature from "./components/Feature/Feature";
-import FlashSale from "./components/FlashSale/FlashSale";
-import Hero from "./components/Hero/Hero";
+import Feature from './components/Feature/Feature';
+import FlashSale from './components/FlashSale/FlashSale';
+import Hero from './components/Hero/Hero';
 
+import { products } from '@/data/products';
+import ProductCard from './components/ProductDetailsPage/ProductCard';
 
 export default function MainHomePage() {
   return (
     <div>
       <Hero />
       <Feature />
+      {/* test for product card  */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
       <FlashSale />
     </div>
-  )
+  );
 }
-
