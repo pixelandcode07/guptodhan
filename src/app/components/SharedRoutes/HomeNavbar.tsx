f141813217a5b36fbe0904d8f9b1fb2cb03760a0
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-09-17 16:07:01 +0600

    feat: Add DonationNavMain to HomeNavbar for donation path handling

--- src/app/components/SharedRoutes/HomeNavbar.tsx
+++ src/app/components/SharedRoutes/HomeNavbar.tsx
@@ -3,15 +3,17 @@ import NavHead from './components/NavHead'
 import NavMain from './components/NavMain'
 import { usePathname } from "next/navigation";
 import BuySellNavMain from './components/BuySellNavMain';
+import DonationNavMain from './components/DonationNavMain';
 
 export default function HomeNavbar() {
     const pathname = usePathname();
 
     const isBuyAndSell = pathname?.startsWith("/home/buyandsell");
+    const isDonation = pathname?.startsWith("/home/donation");
     return (
         <div>
             <NavHead />
-            {isBuyAndSell ? <BuySellNavMain /> : <NavMain />}
+            {isBuyAndSell ? <BuySellNavMain /> : isDonation ? <DonationNavMain /> : <NavMain />}
         </div>
     )
 }
