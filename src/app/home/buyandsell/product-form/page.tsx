4c6d562f89851fa9b7d02b3a47c320ead27b3315
Moinuddin-dotcom <moinchy7@gmail.com>
2025-09-17 19:48:13 +0600

    Buy and sell from section complete

---
+++ src/app/home/buyandsell/product-form/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductForm from '../components/ProductForm'
+
+export default function ProductFormPage() {
+    return (
+        <div>
+            <ProductForm />
+        </div>
+    )
+}
