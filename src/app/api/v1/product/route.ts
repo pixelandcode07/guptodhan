913e35222e29d3397ab8b649ca8b21693e8e5b75
naeem majumder <naeemmajumder00@gmail.com>
2025-09-18 15:46:11 +0600

    role in product

--- src/app/api/v1/product/route.ts
+++ src/app/api/v1/product/route.ts
@@ -1,6 +1,6 @@
 import { VendorProductController } from "@/lib/modules/product/vendorProduct.controller";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
+import { checkRole } from '@/lib/middlewares/checkRole';
 
 export const GET = catchAsync(VendorProductController.getAllVendorProducts);
-
-export const POST = catchAsync(VendorProductController.createVendorProduct);
+export const POST = catchAsync(checkRole(['vendor', 'admin'])(VendorProductController.createVendorProduct));
