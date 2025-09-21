ea25f82d583ea9982cb6f6a652ce9f5d396454ea
Moinuddin-dotcom <moinchy7@gmail.com>
2025-09-21 17:45:22 +0600

    Working on Buy and sell api part

--- src/data/division_wise_locations.ts
+++ src/data/division_wise_locations.ts
@@ -7,45 +7,47 @@ export type DivisionWiseLocations = Division;
 
 export const division_wise_locations: DivisionWiseLocations = {
     "Dhaka Division": {
-        "Dhaka City": ["Dhanmondi", "Gulshan", "Mirpur", "Uttara", "Motijheel"],
-        Gazipur: ["Tongi", "Sreepur", "Kaliakair"],
-        Narayanganj: ["Fatullah", "Sonargaon", "Rupganj"],
+        "Dhaka City": ["Dhanmondi", "Gulshan", "Mirpur", "Uttara", "Motijheel", "Banani", "Bashundhara", "Mohammadpur", "Tejgaon", "Khilgaon", "Rampura", "Shahbagh", "Puran Dhaka", "Badda", "Farmgate", "Kalabagan", "New Market", "Ramna", "Jatrabari", "Uttarkhan"],
+        "Gazipur": ["Tongi", "Sreepur", "Kaliakair", "Kapasia", "Narsingdi"],
+        "Narayanganj": ["Fatullah", "Sonargaon", "Rupganj", "Bandar", "Araihazar"],
+        "Narsingdi": ["Belabo", "Monohardi", "Raipura", "Shibpur"]
     },
     "Chattogram Division": {
-        "Chattogram City": ["Agrabad", "Pahartali", "Halishahar", "Kotwali"],
-        "Cox's Bazar": ["Ukhia", "Teknaf", "Ramu"],
-        Rangamati: ["Baghaichhari", "Kaptai", "Barkal"],
+        "Chattogram City": ["Agrabad", "Pahartali", "Halishahar", "Kotwali", "Nasirabad", "Chandgaon", "Panchlaish", "Khulshi", "GEC", "Patenga", "Bakalia", "Muradpur", "Chawk Bazar", "Anwara", "Bayezid"],
+        "Cox's Bazar": ["Ukhia", "Teknaf", "Ramu", "Kutupalong", "Maheshkhali", "Cox's Bazar Sadar"],
+        "Rangamati": ["Baghaichhari", "Kaptai", "Barkal", "Rajasthali", "Rangamati Sadar"],
+        "Feni": ["Feni Sadar", "Parshuram", "Chhagalnaiya", "Daganbhuiyan"]
     },
     "Sylhet Division": {
-        "Sylhet City": ["Zindabazar", "Amberkhana", "South Surma"],
-        Moulvibazar: ["Sreemangal", "Kulaura"],
-        Habiganj: ["Madhabpur", "Ajmiriganj"],
+        "Sylhet City Division": ["Zindabazar", "Amberkhana", "South Surma", "Kotwali", "Noyagaon", "Bimanbandar", "Shibganj"],
+        "Moulvibazar": ["Sreemangal", "Kulaura", "Barlekha", "Rajnagar"],
+        "Habiganj": ["Madhabpur", "Ajmiriganj", "Baniachang", "Lakhai"]
     },
     "Rajshahi Division": {
-        "Rajshahi City": ["Motihar", "Boalia", "Rajpara"],
-        Pabna: ["Ishwardi", "Chatmohar"],
-        Natore: ["Baraigram", "Singra"],
+        "Rajshahi City": ["Motihar", "Boalia", "Rajpara", "Shah Mokhdum", "Paba", "Godagari"],
+        "Pabna": ["Ishwardi", "Chatmohar", "Bera", "Santhia"],
+        "Natore": ["Baraigram", "Singra", "Bagatipara", "Lalpur"]
     },
     "Khulna Division": {
-        "Khulna City": ["Sonadanga", "Khalishpur", "Daulatpur"],
-        Jessore: ["Jashore Sadar", "Bagherpara"],
-        Satkhira: ["Shyamnagar", "Tala"],
+        "Khulna City": ["Sonadanga", "Khalishpur", "Daulatpur", "Kotwali", "Harintana", "Khalishpur Bazar"],
+        "Jessore": ["Jashore Sadar", "Bagherpara", "Chaugachha", "Keshabpur"],
+        "Satkhira": ["Shyamnagar", "Tala", "Debhata", "Kaliganj"]
     },
     "Barishal Division": {
-        "Barishal City": ["Band Road", "Kaunia"],
-        Bhola: ["Char Fasson", "Lalmohan"],
-        Patuakhali: ["Kuakata", "Dumki"],
+        "Barishal City": ["Band Road", "Kaunia", "Katorbazar", "College Road"],
+        "Bhola": ["Char Fasson", "Lalmohan", "Bhola Sadar", "Borhanuddin"],
+        "Patuakhali": ["Kuakata", "Dumki", "Patuakhali Sadar", "Mirzaganj"]
     },
     "Rangpur Division": {
-        "Rangpur City": ["Modern", "College Para"],
-        Dinajpur: ["Birganj", "Phulbari"],
-        Kurigram: ["Ulipur", "Chilmari"],
+        "Rangpur City": ["Modern", "College Para", "Kochukhet", "Lalbagh", "Haragachh"],
+        "Dinajpur": ["Birganj", "Phulbari", "Dinajpur Sadar", "Parbatipur"],
+        "Kurigram": ["Ulipur", "Chilmari", "Kurigram Sadar", "Phulbari"]
     },
     "Mymensingh Division": {
-        "Mymensingh City": ["Ganginapar", "Charpara"],
-        Jamalpur: ["Islampur", "Sarishabari"],
-        Netrokona: ["Kendua", "Madan"],
-    },
-};
+        "Mymensingh City": ["Ganginapar", "Charpara", "Trishal Road", "Sheshmor", "Kewatkhali"],
+        "Jamalpur": ["Islampur", "Sarishabari", "Madarganj", "Dewanganj"],
+        "Netrokona": ["Kendua", "Madan", "Netrokona Sadar", "Atpara"]
+    }
+}
 
 
