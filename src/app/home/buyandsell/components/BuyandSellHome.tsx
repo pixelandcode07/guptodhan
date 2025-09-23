220d6bffdd0bc817c6f70343ab15b735bda9a73b
Moinuddin-dotcom <moinchy7@gmail.com>
2025-09-23 18:57:54 +0600

    Banner data GET section complete

--- src/app/home/buyandsell/components/BuyandSellHome.tsx
+++ src/app/home/buyandsell/components/BuyandSellHome.tsx
@@ -1,45 +1,30 @@
-// import axios from "axios";
+import axios from "axios";
 import BuyandSellAdds from "./BuyandSellAdds";
 import BuyandSellBanner from "./BuyandSellBanner";
 import BuyandSellItems from "./BuyandSellItems";
 
 
-// const fetchBanner = async () => {
-//     try {
-//         const baseUrl = process.env.NEXTAUTH_URL; // http://localhost:3000
-//         console.log("baseUrl:",baseUrl)
-//         const { data: postedData } = await axios.get(`${baseUrl}/api/v1/classifieds-banners`);
-//         console.log("PostedData:", postedData);
-//         return postedData.data || [];
-//     } catch (error) {
-//         console.error("Error fetching posts:", error);
-//         return [];
-//     }
-// };
-
 const fetchBanner = async () => {
     try {
         const baseUrl = process.env.NEXTAUTH_URL;
-        if (!baseUrl) throw new Error("NEXTAUTH_URL is not defined");
-
-        const res = await fetch(`${baseUrl}/api/v1/classifieds-banners`);
-        if (!res.ok) throw new Error("Failed to fetch banners");
-
-        const data = await res.json();
-        return data.data || [];
-    } catch (err) {
-        console.error("Error fetching banners:", err);
+        console.log("baseUrl:", baseUrl)
+        const { data: postedData } = await axios.get(`${baseUrl}/api/v1/public/classifieds-banners`);
+        console.log("PostedData:", postedData);
+        return postedData.data || [];
+    } catch (error) {
+        console.error("Error fetching posts:", error);
         return [];
     }
 };
 
 
+
+
 export default async function BuyandSellHome() {
     const banner = await fetchBanner();
-    console.log("Banner:", banner)
     return (
         <div className='mt-5 max-w-7xl mx-auto'>
-            <BuyandSellBanner />
+            <BuyandSellBanner banner={banner} />
             <BuyandSellItems />
             <BuyandSellAdds />
         </div>
