db9a25fa406672f241d6fcff115189482d72c178
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-09-24 18:02:13 +0600

    sub cateogry error solve buy & sell :

--- src/app/api/v1/public/classifieds-subcategories/subcategories/route.ts
+++
@@ -1,4 +0,0 @@
-import { catchAsync } from "@/lib/middlewares/catchAsync";
-import { ClassifiedSubCategoryController } from "@/lib/modules/classifieds-subcategory/subcategory.controller";
-
-export const GET = catchAsync(ClassifiedSubCategoryController.getSubCategoriesByParent);
