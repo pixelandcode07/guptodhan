8c63893c9d583847c13d9b3dbe5fa73e1f11319b
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-05 14:47:59 +0600

    needed get api created seo settings

--- src/app/api/v1/seo-settings/[id]/route.ts
+++ src/app/api/v1/seo-settings/[id]/route.ts
@@ -2,8 +2,6 @@ import { SeoSettingsController } from '@/lib/modules/seo-settings/seo.controller
 import { catchAsync } from '@/lib/middlewares/catchAsync';
 import { checkRole } from '@/lib/middlewares/checkRole';
 
-/**
- * @description Update specific SEO settings by its document ID. (Admin Only)
- * @method PATCH
- */
+export const GET = catchAsync(checkRole(['admin'])(SeoSettingsController.getSeoSettingsById));
+
 export const PATCH = catchAsync(checkRole(['admin'])(SeoSettingsController.updateSeoSettings));
