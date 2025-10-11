7163c0368cffecacfc4b892324b60b00a1c66079
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-11 13:23:49 +0600

    Update: src/lib/modules/sslcommerz/sslcommerz.interface.ts

---
+++ src/lib/modules/sslcommerz/sslcommerz.interface.ts
@@ -0,0 +1,9 @@
+export interface ISSLCommerzPayload {
+    total_amount: number;
+    tran_id: string;
+    cus_name: string;
+    cus_email: string;
+    cus_add1: string;
+    cus_phone: string;
+    product_name: string;
+}
