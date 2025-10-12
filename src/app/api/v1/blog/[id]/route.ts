70665b5c09fb146eee3bd64da0748982bee7bf1c
naeem majumder <naeemmajumder00@gmail.com>
2025-10-12 14:27:26 +0600

    blog update

--- src/app/api/v1/blog/[id]/route.ts
+++ src/app/api/v1/blog/[id]/route.ts
@@ -1,6 +1,5 @@
 import { BlogController } from "@/lib/modules/blog/blog.controller";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
 
-export const GET = catchAsync(BlogController.getBlogsByCategory);
 export const PATCH = catchAsync(BlogController.updateBlog);
 export const DELETE = catchAsync(BlogController.deleteBlog);
--- src/lib/modules/blog/blog.model.ts
+++ src/lib/modules/blog/blog.model.ts
@@ -5,7 +5,7 @@ const blogSchema = new Schema<IBlog>(
   {
     blogId: { type: String, unique: true },
     coverImage: { type: String, required: true },
-    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+    category: { type: String, required: true },
     title: { type: String, required: true, trim: true },
     description: { type: String, required: true },
     tags: [{ type: String }],
