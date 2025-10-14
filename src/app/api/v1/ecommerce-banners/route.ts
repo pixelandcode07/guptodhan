139c4ab884ca90ae1f0e0e935bd9f760b196c12e
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-14 21:24:17 +0600

    Update: src/app/api/v1/ecommerce-banners/route.ts

---
+++ src/app/api/v1/ecommerce-banners/route.ts
@@ -0,0 +1,6 @@
+import { EcommerceBannerController } from '@/lib/modules/ecommerce-banner/banner.controller';
+import { catchAsync } from '@/lib/middlewares/catchAsync';
+import { checkRole } from '@/lib/middlewares/checkRole';
+
+export const POST = catchAsync(checkRole(['admin'])(EcommerceBannerController.createBanner));
+export const GET = catchAsync(checkRole(['admin'])(EcommerceBannerController.getAllBanners));
