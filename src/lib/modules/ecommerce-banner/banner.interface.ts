3fa93a55e92f705d2e7bf521af8477ca2c0ec5ac
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-14 21:24:17 +0600

    Update: src/lib/modules/ecommerce-banner/banner.interface.ts

---
+++ src/lib/modules/ecommerce-banner/banner.interface.ts
@@ -0,0 +1,14 @@
+import { Document } from 'mongoose';
+
+export interface IEcommerceBanner extends Document {
+  bannerImage: string; // Cloudinary URL
+  bannerPosition: 'top-homepage' | 'left-homepage' | 'right-homepage' | 'middle-homepage' | 'bottom-homepage' | 'top-shoppage';
+  textPosition: 'left' | 'right';
+  bannerLink?: string;
+  subTitle?: string;
+  bannerTitle: string;
+  bannerDescription?: string;
+  buttonText?: string;
+  buttonLink?: string;
+  status: 'active' | 'inactive';
+}
