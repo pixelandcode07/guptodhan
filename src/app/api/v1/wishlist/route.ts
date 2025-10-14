f99eb997e60c703bcc6ae2f600f32fda3e7101ed
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-10-14 21:46:31 +0600

    wishlist done

--- src/app/api/v1/wishlist/route.ts
+++ src/app/api/v1/wishlist/route.ts
@@ -1,4 +1,19 @@
 import { WishlistController } from "@/lib/wishlist/wishlist.controller";
+import dbConnect from "@/lib/db";
+import { WishlistModel } from "@/lib/wishlist/wishlist.model";
+import { StatusCodes } from "http-status-codes";
+import { sendResponse } from "@/lib/utils/sendResponse";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
+
+export const GET = catchAsync(async () => {
+  await dbConnect();
+  const result = await WishlistModel.find({}).sort({ createdAt: -1 });
+  return sendResponse({
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "All wishlist items retrieved successfully!",
+    data: result,
+  });
+});
 
 export const POST = catchAsync(WishlistController.createWishlist);
