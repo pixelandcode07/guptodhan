a004d5ebf7bf0130fb0127c9c746a8f2d58d988f
naeem majumder <naeemmajumder00@gmail.com>
2025-10-24 15:59:47 +0600

    landing page products api

---
+++ src/app/api/v1/product/offerProduct/route.ts
@@ -0,0 +1,4 @@
+import { VendorProductController } from "@/lib/modules/product/vendorProduct.controller";
+import { catchAsync } from "@/lib/middlewares/catchAsync";
+
+export const GET = catchAsync(VendorProductController.getRunningOfferProducts);
