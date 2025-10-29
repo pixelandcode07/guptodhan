bd56db24ee0a46006f621bfa5f450f41f3261ab5
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-10-29 11:56:43 +0600

    Update: src/app/api/v1/auth/google-login/route.ts

--- src/app/api/v1/auth/google-login/route.ts
+++ src/app/api/v1/auth/google-login/route.ts
@@ -2,4 +2,4 @@ import { AuthController } from '@/lib/modules/auth/auth.controller';
 import { catchAsync } from '@/lib/middlewares/catchAsync';
 
 
-export const POST = catchAsync(AuthController.googleLogin);
+export const POST = catchAsync(AuthController.googleLoginHandler);
