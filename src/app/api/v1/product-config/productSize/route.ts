52560c1059ca07802e619920f48ae181637c5b52
naeem majumder <naeemmajumder00@gmail.com>
2025-10-30 18:41:24 +0600

    get fixed

--- src/app/api/v1/product-config/productSize/route.ts
+++ src/app/api/v1/product-config/productSize/route.ts
@@ -2,5 +2,5 @@ import { ProductSizeController } from "@/lib/modules/product-config/controllers/
 import { catchAsync } from "@/lib/middlewares/catchAsync";
 import { checkRole } from "@/lib/middlewares/checkRole";
 
-export const GET = catchAsync(checkRole(["admin"])(ProductSizeController.getAllProductSizes));
+export const GET = catchAsync(ProductSizeController.getAllProductSizes);
 export const POST = catchAsync(checkRole(["admin"])(ProductSizeController.createProductSize));
