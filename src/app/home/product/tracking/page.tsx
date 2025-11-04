2b6a3e79264c5af759ee4a4c9473bded75878a09
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-04 14:39:09 +0600

    Update: src/app/home/product/tracking/page.tsx

--- src/app/home/product/tracking/page.tsx
+++ src/app/home/product/tracking/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react' // 1. Suspense import করুন
 import HeroNav from '@/app/components/Hero/HeroNav'
 import SteadfastTracking from '@/app/home/product/shoppinginfo/components/SteadfastTracking'
+import FancyLoadingPage from '@/app/general/loading' // অথবা যেকোনো loading component
 
 export default function TrackingPage() {
   return (
     <>
       <HeroNav />
-      <SteadfastTracking />
+      {/* 2. SteadfastTracking-কে Suspense দিয়ে wrap করুন */}
+      <Suspense fallback={<FancyLoadingPage />}> 
+        <SteadfastTracking />
+      </Suspense>
     </>
   )
 }
