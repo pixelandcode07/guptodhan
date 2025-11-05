4b1aa303184cc1b3454d96ca9a73b6319b705c65
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-05 14:18:19 +0600

    Update: src/app/api/v1/public/search/route.ts

--- src/app/api/v1/public/search/route.ts
+++
@@ -1,8 +0,0 @@
-import { ClassifiedAdController } from '@/lib/modules/classifieds/ad.controller';
-import { catchAsync } from '@/lib/middlewares/catchAsync';
-
-/**
- * @description Search and filter classified ads. (Public)
- * @method GET
- */
-export const GET = catchAsync(ClassifiedAdController.searchAds);
