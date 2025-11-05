b52168a44a423e093d889deb7942ac7b7d9e572b
AyshaUrmi0 <ayshaismail021@gmail.com>
2025-11-06 01:54:47 +0600

    wishlist route handler for userId

--- src/app/api/v1/wishlist/user/[userId]/route.ts
+++ src/app/api/v1/wishlist/user/[userId]/route.ts
@@ -2,5 +2,4 @@ import { WishlistController } from "@/lib/wishlist/wishlist.controller";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
 
 export const GET = catchAsync(WishlistController.getAllWishlist);
-export const PATCH = catchAsync(WishlistController.updateWishlist);
-export const DELETE = catchAsync(WishlistController.deleteWishlist);
+
