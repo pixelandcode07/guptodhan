a9c3d69c731e3fde1c225a1de83bc8f6648d3628
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-07 22:36:57 +0600

    Update: src/app/api/v1/payment/init/route.ts

--- src/app/api/v1/payment/init/route.ts
+++ src/app/api/v1/payment/init/route.ts
@@ -1,3 +1,9 @@
+// ==========================================
+// FILE: app/api/v1/payment/init/route.ts
+// ==========================================
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
 import { PaymentController } from '@/lib/modules/payment/payment.controller';
 import { catchAsync } from '@/lib/middlewares/catchAsync';
 import { checkRole, TUserRole } from '@/lib/middlewares/checkRole';
