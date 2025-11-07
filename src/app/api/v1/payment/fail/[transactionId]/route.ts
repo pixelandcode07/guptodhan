ba906d0ab46bf862b809a605a78ac98641eed0a7
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-07 22:36:56 +0600

    Update: src/app/api/v1/payment/fail/[transactionId]/route.ts

--- src/app/api/v1/payment/fail/[transactionId]/route.ts
+++ src/app/api/v1/payment/fail/[transactionId]/route.ts
@@ -1,2 +1,10 @@
+// ==========================================
+// FILE: app/api/v1/payment/fail/[transactionId]/route.ts
+// ==========================================
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
 import { PaymentController } from '@/lib/modules/payment/payment.controller';
-export const POST = PaymentController.handleFail;
+import { catchAsync } from '@/lib/middlewares/catchAsync';
+
+export const GET = catchAsync(PaymentController.handleFail);
