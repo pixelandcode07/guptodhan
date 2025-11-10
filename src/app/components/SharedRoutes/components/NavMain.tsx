4952529821bf5f36bc3caf0c2dace142ca9fab7f
Moinuddin-dotcom <moinchy7@gmail.com>
2025-11-11 00:01:06 +0600

    Refactor import statements for consistency and readability

--- src/app/components/SharedRoutes/components/NavMain.tsx
+++ src/app/components/SharedRoutes/components/NavMain.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import {  Heart, LogOut, Menu, User, X, ShoppingBag, Gift, UserPlus, Truck, Phone, LogIn } from 'lucide-react';
+import { Heart, LogOut, Menu, User, X, ShoppingBag, Gift, UserPlus, Truck, Phone, LogIn } from 'lucide-react';
 import Image from 'next/image';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import LogInRegister from '../../LogInAndRegister/LogIn_Register';
 import SearchBar from './SearchBar';
 import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import {  useState } from 'react';
+import { useState } from 'react';
 import CartIcon from '@/components/CartIcon';
 import WishlistIcon from '@/components/WishlistIcon';
 import { motion, AnimatePresence } from 'framer-motion';
