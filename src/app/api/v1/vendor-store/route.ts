0dda933652079772917f8985c9313d1137a50984
Yeamin Madbor <yeaminstudent5598@gmail.com>
2025-11-25 14:34:41 +0600

    Update: src/app/api/v1/vendor-store/route.ts

--- src/app/api/v1/vendor-store/route.ts
+++ src/app/api/v1/vendor-store/route.ts
@@ -1,4 +1,4 @@
-import { VendorStoreController } from "@/lib/modules/vendor-store/vendorStore.controller";
+    import { VendorStoreController } from "@/lib/modules/vendor-store/vendorStore.controller";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
 
 export const GET = catchAsync(VendorStoreController.getAllStores);
