a4afc2c7deffebed868d713de490ddb2232f7ea4
Moinuddin-dotcom <moinchy7@gmail.com>
2025-12-04 19:08:15 +0600

    feat: Implement vendor store features with sticky navigation and enhanced UI components

--- src/app/home/buyandsell/layout.tsx
+++ src/app/home/buyandsell/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'User Profile',
 }
 
-export default function UserProfileLayout({ children }: { children: React.ReactNode }) {
+export default function BuyAndSellLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="">
       {/* flex gap-6 max-w-7xl mx-auto w-full px-4 py-6 */}
--- src/lib/MultiVendorApis/fetchAllStore.ts
+++ src/lib/MultiVendorApis/fetchAllStore.ts
@@ -23,3 +23,21 @@ export const fetchAllStores = async (): Promise<StoreInterface[]> => {
     throw new Error(error.response?.data?.message || 'Failed to fetch stores');
   }
 };
+
+
+
+export const fetchAllPublicStores = async (): Promise<StoreInterface[]> => {
+  const baseUrl = process.env.NEXTAUTH_URL;
+
+  if (!baseUrl) {
+    console.error('API base URL is not configured');
+    return [];
+  }
+  try {
+    const response = await axios.get<ApiResponse>(`${baseUrl}/api/v1/public/vendor-store`);
+    return response.data.data;
+  } catch (error: any) {
+    console.error('Axios Error: Failed to fetch stores', error.message || error);
+    throw new Error(error.response?.data?.message || 'Failed to fetch stores');
+  }
+};
