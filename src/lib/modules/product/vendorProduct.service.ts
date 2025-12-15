f49b4050bbaa94fdcb0b26370c70bbd1c2907b33
Moinuddin-dotcom <moinchy7@gmail.com>
2025-12-15 21:46:33 +0600

    search filter backend update

--- src/lib/modules/product/vendorProduct.service.ts
+++ src/lib/modules/product/vendorProduct.service.ts
@@ -61,7 +61,7 @@ const getVendorProductByIdFromDB = async (id: string) => {
     .populate("weightUnit", "name")
     .populate("vendorStoreId", "storeName storeLogo")
     .lean(); // Convert to plain JavaScript object
-    
+
 
   if (!productDoc) return null;
 
@@ -444,7 +444,10 @@ const getLiveSuggestionsFromDB = async (searchTerm: string) => {
     status: "active",
     productTitle: { $regex: regex },
   })
-    .select("productTitle productImage price") // Only necessary fields
+    .select("productTitle productImage price") // -> Add By Moinuddin
+    .populate("category", "slug") // -> Add By Moinuddin
+    .populate("subCategory", "slug") // -> Add By Moinuddin
+    .populate("childCategory", "slug") // -> Add By Moinuddin
     .limit(5)
     .sort({ createdAt: -1 });
 
