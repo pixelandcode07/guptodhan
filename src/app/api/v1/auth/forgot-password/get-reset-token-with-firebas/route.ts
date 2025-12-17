bc63fbf19cb317fb3f1cbeb665403fdcb2d636ca
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-12-17 22:17:09 +0600

    Update: src/app/api/v1/auth/forgot-password/get-reset-token-with-firebas/route.ts

--- src/app/api/v1/auth/forgot-password/get-reset-token-with-firebas/route.ts
+++
@@ -1,5 +0,0 @@
-// ফাইল পাথ: D:\yeamin student\Guptodhan Project\guptodhan\src\app\api\auth\forgot-password\get-reset-token-with-firebase\route.ts
-import { AuthController } from '@/lib/modules/auth/auth.controller';
-import { catchAsync } from '@/lib/middlewares/catchAsync';
-
-export const POST = catchAsync(AuthController.getResetTokenWithFirebase);
