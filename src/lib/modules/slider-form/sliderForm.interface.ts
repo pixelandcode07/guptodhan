b45427de721cd674fc028dbf1733f0ecff60037b
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-12-18 15:54:22 +0600

    Update: src/lib/modules/slider-form/sliderForm.interface.ts

--- src/lib/modules/slider-form/sliderForm.interface.ts
+++ src/lib/modules/slider-form/sliderForm.interface.ts
@@ -3,21 +3,28 @@ export interface IPKSlider {
   sliderId: string;
   image: string;
   textPosition: string;
-  
+
   // Web
   sliderLink: string;
   buttonLink: string;
-  
-  // App
+
+  // App Navigation
   appRedirectType?: 'Product' | 'Category' | 'Brand' | 'Shop' | 'ExternalUrl' | 'None';
-  appRedirectId?: string;
-  
+  // 🔥 FIX: null এবং undefined দুইটাই এলাউ করা হয়েছে
+  appRedirectId?: string | null;
+
+  // অ্যাপ ডেভেলপারের জন্য ফিল্ডস
+  actionStatus: 'product' | 'category' | 'store' | 'none';
+  productId?: string | null;
+  category?: string | null;
+  store?: string | null;
+
   subTitleWithColor: string;
   bannerTitleWithColor: string;
   bannerDescriptionWithColor: string;
   buttonWithColor: string;
-  
-  status?: string;
+
+  status?: 'active' | 'inactive';
   orderCount?: number;
   createdAt?: Date;
   updatedAt?: Date;
