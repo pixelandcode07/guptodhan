efe80cf77f9d09569a0d7d09ac62b294d738aebe
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-05 16:53:32 +0600

    Update: src/app/general/layout.tsx

--- src/app/general/layout.tsx
+++ src/app/general/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 import AppSidebar from '@/components/DashboardComponent/AppSidebar';
 import DashNavbar from '@/components/DashboardComponent/DashNavbar';
 import { SidebarProvider } from '@/components/ui/sidebar';
