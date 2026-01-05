e7146f1e049338cffcbf3f3e3305103701fce696
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-01-05 16:53:30 +0600

    Update: src/app/api/v1/donation-campaigns/[id]/route.ts

--- src/app/api/v1/donation-campaigns/[id]/route.ts
+++ src/app/api/v1/donation-campaigns/[id]/route.ts
@@ -1,12 +1,9 @@
-import { DonationCampaignController } from '@/lib/modules/donation-campaign/donation-campaign.controller';
-import { catchAsync } from '@/lib/middlewares/catchAsync';
-import { checkRole } from '@/lib/middlewares/checkRole';
+import { catchAsync } from "@/lib/middlewares/catchAsync";
+import { checkRole } from "@/lib/middlewares/checkRole";
+import { DonationCampaignController } from "@/lib/modules/donation-campaign/donation-campaign.controller";
 
-// GET Single Campaign
 export const GET = catchAsync(DonationCampaignController.getCampaignById);
-
-// UPDATE Campaign (Only Admin or specific logic)
-export const PATCH = catchAsync(checkRole(['admin', 'user'])(DonationCampaignController.updateCampaign));
-
-// DELETE Campaign (Only Admin)
-export const DELETE = catchAsync(checkRole(['admin'])(DonationCampaignController.deleteCampaign));
+export const PATCH = catchAsync(DonationCampaignController.updateCampaign);
+export const DELETE = catchAsync(
+  checkRole(['admin'])(DonationCampaignController.deleteCampaign)
+);
