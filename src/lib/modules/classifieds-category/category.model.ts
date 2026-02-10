31739782a8a4afbd2411c8f999f2fe1db0d0bb20
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-02-10 01:15:04 -0800

    Update: src/lib/modules/classifieds-category/category.model.ts

--- src/lib/modules/classifieds-category/category.model.ts
+++ src/lib/modules/classifieds-category/category.model.ts
@@ -3,6 +3,7 @@ import { IClassifiedCategory } from './category.interface';
 
 const classifiedCategorySchema = new Schema<IClassifiedCategory>({
   name: { type: String, required: true, unique: true },
+  slug: { type: String, required: true, unique: true },
   icon: { type: String },
   status: { type: String, enum: ['active', 'inactive'], default: 'active' },
   orderCount:{type:Number, required: false, default: 0}
