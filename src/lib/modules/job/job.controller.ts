115d48414ca149ef606ea47de67446babd099f96
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-02-09 20:37:15 -0800

    Update: src/lib/modules/job/job.controller.ts

--- src/lib/modules/job/job.controller.ts
+++ src/lib/modules/job/job.controller.ts
@@ -4,8 +4,9 @@ import dbConnect from "@/lib/db";
 import { JobService } from "./job.service";
 import { jobZodSchema, updateStatusZodSchema } from "./job.validation";
 import { catchAsync } from "@/lib/middlewares/catchAsync";
+import { isValidObjectId } from "mongoose";
 
-// Admin: Get All Jobs (Pending, Approved, Rejected)
+// Admin: Get All Jobs
 const getAllJobsForAdmin = catchAsync(async () => {
   await dbConnect();
   const result = await JobService.getAllJobsForAdminFromDB();
@@ -22,7 +23,12 @@ const getPublicJobs = catchAsync(async () => {
 // User: Create Job
 const createJob = catchAsync(async (req: NextRequest) => {
   await dbConnect();
+  
   const userId = req.headers.get('x-user-id');
+  if (!userId) {
+    return NextResponse.json({ success: false, message: "User not authenticated" }, { status: 401 });
+  }
+
   const body = await req.json();
   const validatedData = jobZodSchema.parse(body);
 
@@ -38,24 +44,32 @@ const createJob = catchAsync(async (req: NextRequest) => {
 const updateStatus = catchAsync(async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
   await dbConnect();
   const { id } = await params;
+
+  if (!isValidObjectId(id)) {
+    return NextResponse.json({ success: false, message: "Invalid Job ID" }, { status: 400 });
+  }
+
   const { status } = updateStatusZodSchema.parse(await req.json());
   const result = await JobService.updateJobStatusInDB(id, status);
+
+  if (!result) return NextResponse.json({ success: false, message: "Job not found" }, { status: 404 });
+
   return NextResponse.json({ success: true, data: result });
 });
 
-
+// Single Job Details
 const getSingleJob = catchAsync(async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
   await dbConnect();
-  
   const { id } = await params;
+
+  if (!isValidObjectId(id)) {
+    return NextResponse.json({ success: false, message: "Invalid Job ID format" }, { status: 400 });
+  }
 
   const result = await JobService.getSingleJobByIdFromDB(id);
 
   if (!result) {
-    return NextResponse.json(
-      { success: false, message: "Job not found or not approved yet!" },
-      { status: StatusCodes.NOT_FOUND }
-    );
+    return NextResponse.json({ success: false, message: "Job not found or not approved yet!" }, { status: 404 });
   }
 
   return NextResponse.json({
