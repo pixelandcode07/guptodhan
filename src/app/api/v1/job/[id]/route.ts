fd5ce614ba86779052e4cefe8d6af436156c2860
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-02-09 20:37:14 -0800

    Update: src/app/api/v1/job/[id]/route.ts

--- src/app/api/v1/job/[id]/route.ts
+++ src/app/api/v1/job/[id]/route.ts
@@ -1,4 +1,5 @@
 import { checkRole } from "@/lib/middlewares/checkRole";
 import { JobController } from "@/lib/modules/job/job.controller";
 
+export const GET = checkRole(['admin'])(JobController.getSingleJob)
 export const PATCH = checkRole(['admin'])(JobController.updateStatus);
