f30d8016d39f1921e373d6825f94df54691074ba
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-02-19 16:02:57 +0600

    Update: src/app/api/v1/notification/send/route.ts

--- src/app/api/v1/notification/send/route.ts
+++
@@ -1,4 +0,0 @@
-import { NotificationController } from '@/lib/modules/notification/notification.controller';
-import { catchAsync } from '@/lib/middlewares/catchAsync';
-
-export const POST = catchAsync(NotificationController.sendBroadcastNotification);
