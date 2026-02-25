0507d66a0bc98461c161007eba79f2073bc31f42
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-02-25 16:19:34 +0600

    Update: src/app/api/v1/public/product/route.ts

--- src/app/api/v1/public/product/route.ts
+++ src/app/api/v1/public/product/route.ts
@@ -2,4 +2,4 @@ import { catchAsync } from "../../../../../lib/middlewares/catchAsync";
 import { VendorProductController } from "../../../../../lib/modules/product/vendorProduct.controller";
 
 // Ekhon "getAllVendorProductsNoPagination" function ta call koro
-export const GET = catchAsync(VendorProductController.getAllVendorProductsNoPagination);
+export const GET = catchAsync(VendorProductController.getAllVendorProductsWithPagination);
