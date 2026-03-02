37139bd0ed805bc02bcea3b53a36160edb09eb7a
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-03-02 13:15:00 +0600

    Update: src/lib/modules/notification/notification.model.ts

--- src/lib/modules/notification/notification.model.ts
+++ src/lib/modules/notification/notification.model.ts
@@ -1,9 +1,10 @@
 import { Schema, model, models } from 'mongoose';
 
 const notificationSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User', default: null }, 
   title: { type: String, required: true },
   message: { type: String, required: true },
-  image: { type: String }, // অপশনাল
+  image: { type: String },
   type: { type: String, enum: ['broadcast', 'individual'], default: 'broadcast' },
   status: { type: String, enum: ['sent', 'failed'], default: 'sent' },
   sentAt: { type: Date, default: Date.now }
