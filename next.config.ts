28c447f495a7b0c3d08cfc6393ea1c30c3b8915f
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-03-05 16:19:26 +0600

    Update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,7 @@ const nextConfig: NextConfig = {
       // Image hosts
       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
       { protocol: 'http', hostname: '76.13.191.238', pathname: '/**' },
+      { protocol: 'https', hostname: 'cdn.guptodhan.com', pathname: '/**' },
       { protocol: 'https', hostname: 'i.ibb.co', pathname: '/**' },
       { protocol: 'https', hostname: 'i.ibb.co.com', pathname: '/**' },
       { protocol: 'https', hostname: 'i.imgur.com', pathname: '/**' },
