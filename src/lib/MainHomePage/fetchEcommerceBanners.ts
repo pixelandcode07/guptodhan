469c1bd9c3b81a45a1de98f9e78d4a7dbb9796ed
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-03-25 14:17:08 +0600

    Update: src/lib/MainHomePage/fetchEcommerceBanners.ts

--- src/lib/MainHomePage/fetchEcommerceBanners.ts
+++ src/lib/MainHomePage/fetchEcommerceBanners.ts
@@ -1,10 +1,6 @@
-
-
-
-// src/lib/MainHomePage/fetchEcommerceBanners.ts
-
 import axios, { AxiosError } from 'axios';
 import { ApiResponse, EcommerceBannerType, EcommerceSliderBannerType } from '@/types/ecommerce-banner-type';
+import { unstable_noStore as noStore } from 'next/cache'; // <-- Added here
 
 interface EcommerceBannersResponse {
   leftBanners: EcommerceSliderBannerType[];
@@ -15,6 +11,8 @@ interface EcommerceBannersResponse {
 }
 
 export async function fetchEcommerceBanners(): Promise<EcommerceBannersResponse> {
+  noStore(); // <-- Added to prevent Next.js server caching
+
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
   if (!baseUrl) {
