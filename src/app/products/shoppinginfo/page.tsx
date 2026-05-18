dda7b7a3a96251e679c0e294bc9fb0d9df4cdd8d
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-05-18 14:49:05 +0600

    Update: src/app/products/shoppinginfo/page.tsx

--- src/app/products/shoppinginfo/page.tsx
+++ src/app/products/shoppinginfo/page.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
 import ShoppingInfoClient from './components/ShoppingInfoClient';
-import { HeroNav } from '@/app/components/Hero/HeroNav';
-import { MainCategory } from '@/types/navigation-menu';
 
-export default function ShoppingInfoPage() {
-    const categoriesData: MainCategory[] = [];
 
   return (
     <>
-      <HeroNav categories={categoriesData} />
       <div className="mx-auto max-w-7xl px-3 py-4 sm:px-4 sm:py-6">
         <ShoppingInfoClient />
       </div>
