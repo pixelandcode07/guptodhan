624b2d785d04cf38ff42acc375b14ed20fe81a96
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-06-17 15:55:36 +0600

    Update: src/app/general/buy/sell/listing/page.tsx

--- src/app/general/buy/sell/listing/page.tsx
+++ src/app/general/buy/sell/listing/page.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { buySellListing_columns } from '@/components/TableHelper/buySellListing_columns'
 import { DataTable } from '@/components/TableHelper/data-table'
-import { fetchAllClassifiedAdsForAdmin } from '@/lib/BuyandSellApis/fetchClassifiedAds' // ✅ Updated Import
+import { fetchClassifiedAds } from '@/lib/BuyandSellApis/fetchClassifiedAds'
 import { ClassifiedAdListing } from '@/types/ClassifiedAdsType'
 import { getServerSession } from 'next-auth'
 
@@ -9,9 +9,10 @@ export default async function BuySellListing() {
     const session = await getServerSession(authOptions)
     const token = session?.accessToken as string | undefined;
     
-    // ✅ ডাটা ফেচ করা হচ্ছে অ্যাডমিন API থেকে (Pending সহ সব আসবে)
-    const rawListing: ClassifiedAdListing[] = await fetchAllClassifiedAdsForAdmin(token)
+    // ডাটা ফেচ করা হচ্ছে
+    const rawListing: ClassifiedAdListing[] = await fetchClassifiedAds(token)
     
+
     const safeListing = Array.isArray(rawListing) 
         ? rawListing.filter(ad => ad && ad.user !== null && ad.user !== undefined)
         : [];
