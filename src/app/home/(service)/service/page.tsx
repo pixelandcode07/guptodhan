4bffdab4d76f30ba47818f96ff51f1b2e2e195c8
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-06-17 16:15:51 +0600

    Update: src/app/home/(service)/service/page.tsx

--- src/app/home/(service)/service/page.tsx
+++ src/app/home/(service)/service/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import React, { Suspense } from 'react'
 import ServiceBanner from '../components/ServiceBanner'
 import ServiceCards from '../components/ServiceCards'
