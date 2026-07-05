4bed2171bd8ef7f125422898918532d684add583
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-07-05 12:24:48 +0600

    Update: src/app/home/UserProfile/reviews/Components/TabsSection.tsx

--- src/app/home/UserProfile/reviews/Components/TabsSection.tsx
+++ src/app/home/UserProfile/reviews/Components/TabsSection.tsx
@@ -55,16 +55,16 @@ export default function TabsSection() {
   return (
     <Tabs defaultValue="toReview" className="w-full">
       <TabsList className="mb-4">
-        <TabsTrigger value="toReview">To be Review</TabsTrigger>
         <TabsTrigger value="history">History</TabsTrigger>
+        <TabsTrigger value="toReview">To be Review</TabsTrigger>
       </TabsList>
 
       <TabsContent value="toReview">
-        <ToBeReview />
+        <ReviewHistory />
       </TabsContent>
 
       <TabsContent value="history">
-        <ReviewHistory />
+        <ToBeReview />
       </TabsContent>
     </Tabs>
   );
