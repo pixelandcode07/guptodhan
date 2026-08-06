1afe5e334ffe10087b15ca9e96d1d136f1b7e73b
Moinuddin-dotcom <moinchy7@gmail.com>
2026-08-06 15:58:35 +0600

    changes

--- src/lib/modules/user/user.controller.ts
+++ src/lib/modules/user/user.controller.ts
@@ -303,6 +303,7 @@ const getMyProfile = async (req: NextRequest) => {
   }
 
   const result = await UserServices.getMyProfileFromDB(userId);
+  console.log("Profile:", result);
 
   return sendResponse({
     success: true,
