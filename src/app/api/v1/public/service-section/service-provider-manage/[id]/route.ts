04d80ff1db8ab3f87d9bdef45f62f499254675b2
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-08-15 15:32:04 +0600

    Update: src/app/api/v1/public/service-section/service-provider-manage/[id]/route.ts

--- src/app/api/v1/public/service-section/service-provider-manage/[id]/route.ts
+++ src/app/api/v1/public/service-section/service-provider-manage/[id]/route.ts
@@ -3,3 +3,4 @@ import { catchAsync } from "@/lib/middlewares/catchAsync";
 
 export const GET = catchAsync(BookingController.getBookingByOrderId);
 export const PATCH = catchAsync(BookingController.updateBooking);
+export const DELETE = catchAsync(BookingController.deleteBooking); // ✅ DELETE API Added
